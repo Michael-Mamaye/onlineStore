eef24078d9eea5d45294294316e5960fd329cd60
Mike <michaelmamaye@gmail.com>
2021-08-11 10:57:35 +0300

    connected with a database using mongoose

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ const express= require('express');
 const PORT=process.env.PORT||5000;
 const app=express();
 
+const mongoose=require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/onlineStore',
+           { useNewUrlParser:true,
+             useUnifiedTopology:true})
+
+const db=mongoose.connection;
+db.once('open',()=>{console.log("successfully connected")})
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
-
-app.use('/miki/api',require('./routes/api/memberlist'))
 
 
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+const uuid=require('uuid')
+
+const storeSchema=new mongoose.Schema({
+    id:{
+        type:String,
+        default:uuid.v4(),
+        unique:true
+    },
+    name:{
+        type:String,
+        required:[true,'what kind of man are you with out a name']
+    },
+    age:{
+        type:Number,
+        default:null
+    },
+    title:{
+        type:String,
+        default:'peasant'
+    }
+})
+
+const User=mongoose.model('User',storeSchema)
+
+module.exports=User;
